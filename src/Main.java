b14d629d7cdd80bda8ea90e1d5648e3f3b6191b3
AnvarPro <anvarvahitov30@rambler.com>
2021-08-14 10:53:39 +0500

    Bonusmile

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 5000;
+        int fixedPriceBonusPerMile = 20;
+        int numberOfBonusMilesEarned = price / fixedPriceBonusPerMile;
+        System.out.println(numberOfBonusMilesEarned);
+
+    }
+}
+
